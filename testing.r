955c34f988bce8db396b49bca528985192eea6e3
Rehaan Khan <rehansarwar134@gmail.com>
2021-01-19 20:15:48 +0500

    few more lined added

--- testing.r
+++ testing.r
@@ -1,3 +1,10 @@
 # This is from GitHub
 print("Hello from GitHub")
 
+name = "Rehan"
+version = "Rstudio in writing"
+
+print(name)
+print("These line is written in Rstudio")
+test = 00
+print(test)
